16fb5fa33fed0f3f064cfafcf966b57a8a49b204
Erlael <kuran001@mail.ru>
2020-03-16 23:28:12 +0700

    Make clang-format

--- geometry.c
+++ geometry.c
@@ -1,66 +1,67 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
-int fpt (int x1, int y1, int x2, int y2, int x3, int y3){
-int Pt;
-Pt = (sqrt(pow(x1 - y1, 2)+ pow(x2 - y2, 2) + pow(x3 - y3, 2)));
-return Pt;
-}
-
-int fst (int x1, int y1, int x2, int y2, int x3, int y3){
-int St;
-St = ((x2 - x1)*(y3 - y1) - (x3 - x1)*(y2 - y1))/2;
-return St;
-}
-
-float fpc (int r){
-float Pc;
-Pc = 2 * 3.14 * r;
-return Pc;
-}
-
-float fsc (int r){
-float Sc;
-Sc = 3.14 * pow(r, 2);
-return Sc;
-}
-
-int main ()
+int fpt(int x1, int y1, int x2, int y2, int x3, int y3)
 {
-int n, i;
-char m1[80] ="triangle";
-char m2[80]= "circle";
-char m3[80];
-int a1, a2, b1, b2, c1,c2,r,S, P;
-printf ("How many figures do you work with?");
-scanf( "%d", &n);
-printf("Enter figures: \n");
-for (i=1; i<=n; i++){
-scanf ("%s", m3);
-if ( strcmp (m1, m3) ==0)
-{
-printf("Enter the vertices");
-scanf ("%d %d %d %d %d %d", &a1 ,&a2, &b1, &b2, &c1, &c2);
-S=fst(a1,a2,b1,b2,c1,c2);
-P=fpt(a1,a2,b1,b2,c1,c2);
-if (S<0)
-S=-S;
-printf("%d Triangle \n S:%d \n P:%d",i,S,P );
-}
-else if(strcmp(m2,m3)==0){
-printf ("Enter center and radius");
-scanf("%d", &a1);
-scanf("%d",&r);
-S=fsc(r);
-P=fpc(r);
-printf("%d Circle \n S:%d \n P:%d", i,S,P);
-}
-else {
-printf ("Error");
-}
+    int Pt;
+    Pt = (sqrt(pow(x1 - y1, 2) + pow(x2 - y2, 2) + pow(x3 - y3, 2)));
+    return Pt;
 }
 
-return 0;
+int fst(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+    int St;
+    St = ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2;
+    return St;
+}
+
+float fpc(int r)
+{
+    float Pc;
+    Pc = 2 * 3.14 * r;
+    return Pc;
+}
+
+float fsc(int r)
+{
+    float Sc;
+    Sc = 3.14 * pow(r, 2);
+    return Sc;
+}
+
+int main()
+{
+    int n, i;
+    char m1[80] = "triangle";
+    char m2[80] = "circle";
+    char m3[80];
+    int a1, a2, b1, b2, c1, c2, r, S, P;
+    printf("How many figures do you work with?");
+    scanf("%d", &n);
+    printf("Enter figures: \n");
+    for (i = 1; i <= n; i++) {
+        scanf("%s", m3);
+        if (strcmp(m1, m3) == 0) {
+            printf("Enter the vertices");
+            scanf("%d %d %d %d %d %d", &a1, &a2, &b1, &b2, &c1, &c2);
+            S = fst(a1, a2, b1, b2, c1, c2);
+            P = fpt(a1, a2, b1, b2, c1, c2);
+            if (S < 0)
+                S = -S;
+            printf("%d Triangle \n S:%d \n P:%d", i, S, P);
+        } else if (strcmp(m2, m3) == 0) {
+            printf("Enter center and radius");
+            scanf("%d", &a1);
+            scanf("%d", &r);
+            S = fsc(r);
+            P = fpc(r);
+            printf("%d Circle \n S:%d \n P:%d", i, S, P);
+        } else {
+            printf("Error");
+        }
+    }
+
+    return 0;
 }
