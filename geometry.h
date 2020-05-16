3d29958bcc3ad897ae873be125dc5ae36e9380ae
Erlael <kuran001@mail.ru>
2020-05-16 21:40:52 +0700

    Edit geometry.h

---
+++ geometry.h
@@ -0,0 +1,10 @@
+#define GEOMETRY_H
+
+
+int fpt(int x1, int y1, int x2, int y2, int x3, int y3);
+
+int fst(int x1, int y1, int x2, int y2, int x3, int y3);
+
+float fpc(int r);
+
+float fsc(int r);
